28c12d0637b21dfec91974778bfd38f3d24899f5
vitaly kovalev <apachai979@gmail.com>
2024-04-29 23:02:55 +0700

    save

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'dist',
+   output: 'export',
     reactStrictMode: true,
     env: {
         API_KEY_BITRIX: process.env.API_KEY_BITRIX,

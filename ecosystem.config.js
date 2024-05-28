5e781648702c7d5f6e758190a8f8e841efbb26b4
apachai979 <apachai979@gmail.com>
2024-05-28 19:17:42 +0700

    ...

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       repo: 'git@github.com:Apachai979/onestep.git',
       path: '/home/onestep',
       'pre-deploy-local': '',
-      'post-deploy': ' source ~/.nmv/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': ' source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }

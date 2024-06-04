63e555f9d2f01e46bbb380394c2cd0cabf8ae2ad
apachai979 <apachai979@gmail.com>
2024-06-04 18:48:12 +0700

    ...

--- app/authorize/page.jsx
+++ app/authorize/page.jsx
@@ -1,36 +1,54 @@
 'use client'
-import { signIn, signOut, useSession } from "next-auth/react"
-import Image from "next/image"
-import Link from "next/link"
+import { signIn, signOut, useSession } from 'next-auth/react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function SignIn() {
     return (
-        <div className="container mx-auto px-4 max-w-[1200px] h-screen">
-            <div className="flex justify-center items-center">
+        <div className="container mx-auto h-screen max-w-[1200px] px-4">
+            <div className="flex items-center justify-center">
                 <Image
                     src="/logoOneStep.png"
                     alt="Profile"
-                    className="object-contain w-48 h-48 rounded-full"
+                    className="h-48 w-48 rounded-full object-contain"
                     width={512}
                     height={512}
                 />
-
             </div>
-
-            <div className="flex justify-center items-center">
-                <div className="flex flex-col justify-center items-center w-96 space-y-2">
-                    <h1 className="text-2xl text-night_green font-semibold text-center p-4">С возвращением</h1>
-                    <input type="email" className="h-12 w-80 border border-gray-300 rounded-lg" placeholder=" Адрес электронной почты" />
-                    <button className="px-1 w-80 h-10 border border-gray-300 rounded-lg">Продолжить</button>
-                    <p>У вас нет учетной записи? <Link href="#">Зарегистрироваться</Link></p>
-                    <div className="flex justify-center items-center w-96">
-                        <hr className="h-0.5 bg-gray-300 w-full" />
+            <div className="flex items-center justify-center">
+                <div className="flex w-96 flex-col items-center justify-center space-y-2">
+                    <h1 className="p-4 text-center text-2xl font-semibold text-night_green">
+                        С возвращением
+                    </h1>
+                    <input
+                        type="email"
+                        className="h-12 w-80 rounded-lg border border-gray-300"
+                        placeholder=" Адрес электронной почты"
+                    />
+                    <button className="h-10 w-80 rounded-lg border border-gray-300 px-1">
+                        Продолжить
+                    </button>
+                    <p>
+                        У вас нет учетной записи? <Link href="#">Зарегистрироваться</Link>
+                    </p>
+                    <div className="flex w-96 items-center justify-center">
+                        <hr className="h-0.5 w-full bg-gray-300" />
                         <p className="px-2">или</p>
-                        <hr className="h-0.5 bg-gray-300 w-full" />
+                        <hr className="h-0.5 w-full bg-gray-300" />
                     </div>
                     <div className="w-80 space-y-2 pb-2 pt-1">
-                        <button onClick={() => signIn('google')} className="px-1 w-full h-10 border border-gray-300 rounded-lg">Google</button>
-                        <button onClick={() => signIn('github')} className="px-1 w-full h-10 border border-gray-300 rounded-lg">Github</button>
+                        <button
+                            onClick={() => signIn('google')}
+                            className="h-10 w-full rounded-lg border border-gray-300 px-1"
+                        >
+                            Google
+                        </button>
+                        <button
+                            onClick={() => signIn('github')}
+                            className="h-10 w-full rounded-lg border border-gray-300 px-1"
+                        >
+                            Github
+                        </button>
                     </div>
                 </div>
             </div>
--- components/AuthComponent.jsx
+++ components/AuthComponent.jsx
@@ -1,38 +1,49 @@
 'use client'
-import { signIn, signOut, useSession } from "next-auth/react"
-import Image from "next/image";
-import Link from "next/link";
+import { signIn, signOut, useSession } from 'next-auth/react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function AuthComponent() {
-    const { data: session } = useSession();
+    const { data: session } = useSession()
 
     console.log(session)
     return (
-        <div className='container mx-auto px-4'>
-
+        <div className="container mx-auto px-4">
             {session ? (
-                <div className="flex justify-end items-center space-x-1">
-
-                    <h1>Добро пожаловать, <Link href="#" onClick={() => signOut({ callbackUrl: "/" })}>{session?.user?.name}</Link></h1>
-                    {session?.user?.image && <Image
-                        src={session.user.image}
-                        alt="Profile"
-                        className="object-contain w-6 h-6 rounded-full"
-                        width={512}
-                        height={512}
-                    />
-                    }
+                <div className="flex items-center justify-end space-x-1">
+                    <h1>
+                        Добро пожаловать,{' '}
+                        <Link href="#" onClick={() => signOut({ callbackUrl: '/' })}>
+                            {session?.user?.name}
+                        </Link>
+                    </h1>
+                    {session?.user?.image && (
+                        <Image
+                            src={session.user.image}
+                            alt="Profile"
+                            className="h-6 w-6 rounded-full object-contain"
+                            width={512}
+                            height={512}
+                        />
+                    )}
                 </div>
-            )
-                : (
-                    <div className="space-x-4">
-                        <h1>Youre not logged in</h1>
-                        <button onClick={() => signIn('google')} className="px-1 w-auto h-10 border border-black rounded-lg">Google</button>
-                        <button onClick={() => signIn('github')} className="px-1 w-auto h-10 border border-black rounded-lg">Github</button>
-                    </div>
-                )
-            }
-
-        </div >
+            ) : (
+                <div className="space-x-4">
+                    <h1>Youre not logged in</h1>
+                    <button
+                        onClick={() => signIn('google')}
+                        className="h-10 w-auto rounded-lg border border-black px-1"
+                    >
+                        Google
+                    </button>
+                    <button
+                        onClick={() => signIn('github')}
+                        className="h-10 w-auto rounded-lg border border-black px-1"
+                    >
+                        Github
+                    </button>
+                </div>
+            )}
+        </div>
     )
 }

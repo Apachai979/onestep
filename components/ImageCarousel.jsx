e7b5e80af24e40f1a35f907d6c8bbc658c05f9ad
Apachai979 <apachai979@gmail.com>
2024-07-08 15:23:33 +0700

    save

--- components/ImageCarousel.jsx
+++ components/ImageCarousel.jsx
@@ -6,22 +6,22 @@ import { useState } from "react";
 
 const slides = [
     {
-        id: 1,
+        id: 0,
         alt: '',
         src: '/academy/tabs/lab.jpeg',
     },
     {
-        id: 2,
+        id: 1,
         alt: '',
         src: '/academy/tabs/lab2.jpeg',
     },
     {
-        id: 3,
+        id: 2,
         alt: '',
         src: '/academy/tabs/lab3.jpeg',
     },
     {
-        id: 4,
+        id: 3,
         alt: '',
         src: '/academy/tabs/lab4.jpeg',
     }
@@ -30,82 +30,83 @@ const slides = [
 
 export default function ImageCarousel() {
 
+    const [count, setCount] = useState(0)
+    const [animating, setAnimating] = useState(false);
 
 
-    const [slide, setSlide] = useState(slides[0])
-    console.log(slides[slides.length - 1])
+    console.log(slides.length)
+
     const previousSlide = () => {
-        if (slide.id == slides[0].id) setSlide(slides[slides.length - 1])
-        else setSlide(slides[slide.id - 1])
+        if (!animating) {
+            setAnimating(true);
+            setTimeout(() => {
+                setCount((count - 1 + slides.length) % slides.length);
+                setAnimating(false);
+            }, 300); // Duration of the animation
+        }
     }
 
     const nextSlide = () => {
-        if (slide.id == slides[slides.length - 1].id) setSlide(slides[0])
-        else setSlide(slides[slide.id + 1])
+        if (!animating) {
+            setAnimating(true);
+            setTimeout(() => {
+                setCount((count + 1) % slides.length);
+                setAnimating(false);
+            }, 300); // Duration of the animation
+        }
+    }
+
+    const pickSlide = (index) => {
+        if (!animating) {
+            setAnimating(true);
+            setTimeout(() => {
+                setCount(index);
+                setAnimating(false);
+            }, 300); // Duration of the animation
+        }
     }
 
     return (
         <>
-            <div className="w-[370px] space-y-2">
-                <div className="relative h-[260px] w-[370px]">
+            <div className="w-[370px] space-y-2 select-none">
+                <div className="relative h-[260px] w-[370px] rounded-xl overflow-hidden">
 
-                    <div className="absolute left-0 top-1/2 -translate-y-1/2" onClick={previousSlide}>
-                        <RiArrowLeftWideFill size="60" className="fill-mainGreen" />
+                    <div className="absolute left-0 w-8 h-full group hover:bg-gray-400/50 cursor-pointer" onClick={previousSlide}>
+                        <RiArrowLeftWideFill size="45" className="fill-mainGreen -left-2 absolute top-1/2 -translate-y-1/2 group-hover:scale-90" />
                     </div>
 
-                    <div className="absolute right-0 top-1/2 -translate-y-1/2" onClick={nextSlide}>
-                        <RiArrowRightWideFill size="60" className="fill-mainGreen" />
+                    <div className="absolute right-0 w-8 h-full group hover:bg-gray-400/50 cursor-pointer" onClick={nextSlide}>
+                        <RiArrowRightWideFill size="45" className="fill-mainGreen absolute -right-2 top-1/2 -translate-y-1/2 group-hover:scale-90" />
                     </div>
 
                     <div className="">
                         <Image
-                            src={slide.src}
-                            alt={slide.alt}
+                            src={slides[count].src}
+                            alt={slides[count].alt}
                             width={1920}
                             height={1080}
-                            className=' h-[260px] w-[370px] rounded-xl object-cover shadow-lg'
+                            className={`slide h-[260px] w-[370px] object-cover shadow-lg  ${animating ? 'opacity-10 transition duration-500' : ''}`}
                         ></Image>
                     </div>
 
                 </div>
+                <hr />
                 <div className="flex justify-center space-x-2 mx-4">
-                    {slides.map((el) => {
+                    {slides.map((el, index) => {
+                        const isLastElement = index === slides.length - 1;
                         return (
-                            el.id !== slide.id &&
+                            el.id !== slides[count].id &&
                             <div key={el.id}>
-                                <Image onClick={() => setSlide(el)}
+                                <Image onClick={() => pickSlide(el.id)}
                                     src={el.src}
                                     alt={el.alt}
                                     width={1920}
                                     height={1080}
-                                    className='first:skew-y-3 h-[50px] w-[70px] rounded-md object-cover shadow-sm'
+                                    className={`${index == 0 && 'skew-y-3'} ${index === slides.length - 1 && '-skew-y-3'} h-[50px] w-[70px] rounded-md object-cover shadow-sm cursor-pointer`}
                                 ></Image>
                             </div>
                         )
                     })}
-                </div>
-                <div className="flex justify-center space-x-2 mx-4">
-                    <div><Image
-                        src='/academy/tabs/lab.jpeg'
-                        alt='Nurses'
-                        width={1920}
-                        height={1080}
-                        className='skew-y-3 h-[50px] w-[70px] rounded-md object-cover shadow-sm'
-                    ></Image></div>
-                    <div><Image
-                        src='/academy/tabs/lab2.jpeg'
-                        alt='Nurses'
-                        width={1920}
-                        height={1080}
-                        className='h-[50px] w-[70px] rounded-md object-cover shadow-sm'
-                    ></Image></div>
-                    <div><Image
-                        src='/academy/tabs/lab3.jpeg'
-                        alt='Nurses'
-                        width={1920}
-                        height={1080}
-                        className='-skew-y-3 h-[50px] w-[70px] rounded-md object-cover shadow-sm'
-                    ></Image></div>
                 </div>
             </div>
         </>

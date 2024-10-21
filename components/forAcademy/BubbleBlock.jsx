fa272a6b46b8e57f0b8924cb91d15d0538df7349
Vitalii <apachai979@gmail.com>
2024-10-21 18:32:58 +0700

    it is work!

--- components/forAcademy/BubbleBlock.jsx
+++ components/forAcademy/BubbleBlock.jsx
@@ -1,138 +1,44 @@
 "use client"
-import { useState, useRef } from "react"
 import parsedData from "@/components/Data/dataAcademy.json"
-import React from "react"
-import Image from "next/image"
-import Link from "next/link"
+import { useState } from "react"
+import Box from "@/components/forAcademy/Box"
 
-export default React.memo(function BubbleBlock({ sections, categories, posts }) {
-    const [idCategories, setIdCategories] = useState(categories.map(i => i.id))
-    const [idSecton, setIdSection] = useState(sections.map(i => i.id))
-    const [activeCategory, setActiveCategory] = useState(null)
-    const [activeSection, setActiveSection] = useState(null)
-    const [classname, setClassname] = useState("")
-    const animationRef = useRef(null)
-
-    const sectionMoveToTop = index => {
-        setActiveSection(index)
-        setIdSection([index, ...idSecton.filter(i => i !== index)])
+export default async function Academy() {
+    const [activeTab, setActiveTab] = useState("Менеджмент качества")
+    const handleTabClick = title => {
+        setActiveTab(title)
     }
 
-    const moveToTop = index => {
-        if (animationRef.current) {
-            clearTimeout(animationRef.current) // Отменяем текущую анимацию
-        }
-
-        if (index === activeCategory) {
-            setClassname("animate-shaker")
-            animationRef.current = setTimeout(() => {
-                setClassname("")
-                animationRef.current = null
-            }, 400)
-        } else {
-            setIdCategories([index, ...idCategories.filter(i => i !== index)])
-            setClassname("animate-transformt z-10")
-            animationRef.current = setTimeout(() => {
-                setClassname("")
-                animationRef.current = null
-            }, 700)
-            setActiveCategory(index)
-            setActiveSection(null)
-        }
-    }
+    // Перемещаем активную вкладку в начало списка
+    const sortedData = [
+        ...parsedData.filter(item => item.title === activeTab),
+        ...parsedData.filter(item => item.title !== activeTab),
+    ]
 
     return (
-        <div className='p-4'>
-            <div className='mb-5 flex flex-wrap gap-4'>
-                {categories.map(el => {
-                    const active = activeCategory === el.id
-                    return (
-                        <button
-                            key={el.id}
-                            className={`text-nowrap rounded-full border border-primary_green bg-stone-50 px-3 py-2 text-lg font-semibold text-primary_green ${active && "translate-y-2 shadow-neon"}`}
-                            onClick={() => moveToTop(el.id)}
-                        >
-                            {el.name}
-                        </button>
-                    )
-                })}
+        <>
+            {/* Навигационные элементы */}
+            <div className='mb-5 flex gap-4'>
+                {parsedData.map(item => (
+                    <button
+                        key={item.title}
+                        href='#'
+                        onClick={() => handleTabClick(item.title)}
+                        className={`text-nowrap rounded-full border border-primary_green bg-stone-50 px-3 py-2 text-lg font-semibold ${
+                            activeTab === item.title ? "text-primary_green" : "text-gray-500"
+                        }`}
+                    >
+                        {item.title}
+                    </button>
+                ))}
             </div>
-            <div className='flex flex-col'>
-                {idCategories.map(idCategory => {
-                    const selectPosts = posts.filter(el => el.categoryId === idCategory)
-                    // console.log('-- ', selectPosts)
-                    return (
-                        selectPosts.length > 0 && (
-                            <div
-                                key={idCategory}
-                                className={`my-2 rounded-3xl border bg-zinc-200 p-4 ${idCategories[0] === idCategory && classname}`}
-                            >
-                                <h2 className='py-2 text-xl font-semibold'>
-                                    {categories[idCategory - 1].name}
-                                </h2>
-                                <div className='mb-3 flex gap-3'>
-                                    {selectPosts
-                                        .filter(
-                                            (item, index, self) =>
-                                                index ===
-                                                self.findIndex(
-                                                    t => t.section.name === item.section.name
-                                                )
-                                        )
-                                        .map(post => {
-                                            const active = activeSection === post.section.id
-                                            return (
-                                                <button
-                                                    key={post.id}
-                                                    onClick={() =>
-                                                        sectionMoveToTop(post.section.id)
-                                                    }
-                                                    className={`my-1 rounded-full border border-gray-700 px-3 py-1 ${active && "bg-primary_green"}`}
-                                                >
-                                                    {post.section.name}
-                                                </button>
-                                            )
-                                        })}
-                                </div>
-                                {selectPosts.length > 0 &&
-                                    selectPosts.map(post => {
-                                        // console.log('post: ', post)
 
-                                        return (
-                                            post.section.id === activeSection && (
-                                                <>
-                                                    {/* <div className='font-semibold py-1 '>{post.section.name}</div> */}
-                                                    <div
-                                                        key={post.id}
-                                                        className={`mb-4 rounded-3xl bg-stone-300 p-4`}
-                                                    >
-                                                        <Link
-                                                            href={post.postUrl}
-                                                            className='flex items-center space-x-4'
-                                                        >
-                                                            <div className='h-14 w-14 rotate-45 overflow-hidden rounded-xl'>
-                                                                <Image
-                                                                    src='/logo_only.svg'
-                                                                    alt=''
-                                                                    width={720}
-                                                                    height={480}
-                                                                    className='h-14 w-14 -rotate-45 object-cover object-center'
-                                                                ></Image>
-                                                            </div>
-                                                            <div className='text-xl font-semibold'>
-                                                                {post.title}
-                                                            </div>
-                                                        </Link>
-                                                    </div>
-                                                </>
-                                            )
-                                        )
-                                    })}
-                            </div>
-                        )
-                    )
-                })}
+            {/* Отображение всех блоков с контентом, активный блок первым */}
+            <div className='mb-3 flex flex-col gap-3'>
+                {sortedData.map(item => (
+                    <Box key={item.title} title={item.title} categories={item.categories} />
+                ))}
             </div>
-        </div>
+        </>
     )
-})
+}

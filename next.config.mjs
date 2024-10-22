ea25e5b259810218e0e73a9276aa5d153b7969c7
Vitalii <apachai979@gmail.com>
2024-10-22 21:07:08 +0700

    ыфму

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
+        domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
     },
     // output: "standalone",
     reactStrictMode: true,
     env: {
         API_KEY_BITRIX: process.env.API_KEY_BITRIX,
-    }
-};
+    },
+}
 
-export default nextConfig;
+export default nextConfig

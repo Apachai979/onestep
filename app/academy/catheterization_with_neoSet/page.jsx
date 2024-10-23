9818a2369056f70913e50fade1061088fc4c899f
Vitalii <apachai979@gmail.com>
2024-10-23 22:29:47 +0700

    update image academy

--- app/academy/catheterization_with_neoSet/page.jsx
+++ app/academy/catheterization_with_neoSet/page.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 
-export default function catheterization() {
+export default function catheterization_m() {
     return (
         <>
             <div className='container mx-auto my-8 max-w-[1200px] px-10'>
